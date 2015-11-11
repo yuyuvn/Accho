ee65dc80dd858e0ac3dc096551f4fd5bc153cef0
Clicia Scarlet <yuyuvn@mac.com>
2015-11-12 01:22:25 +0700

    Support amazon japan

--- plugins/check_amazonjp.php
+++ plugins/check_amazonjp.php
@@ -1,9 +1,9 @@
 <?php
-class check_amazon extends plugins {
-	public $id = "amazon";
-	public $name = "Check amazon";
+class check_amazonjp extends plugins {
+	public $id = "amazonjp";
+	public $name = "Check amazon japan";
 	public $version = "1.0.0";
-	public $descrition = "Check amazon acc, points and credit cards";
+	public $descrition = "Check amazon japan acc, points and credit cards";
 
 	public $fields = array(
 		"status" => "str",
@@ -21,34 +21,36 @@ class check_amazon extends plugins {
 
 		$return = array();
 
-		$ref = "https://www.amazon.com/";
+		$ref = "https://www.amazon.co.jp/";
 
 		if (!preg_match('/[\\\'\"](\/gp\/navigation\/redirector\.html\/ref\=sign\-in\-redirect.+?)[\\\'\"]/i',$this->connect($ref),$match)) {
 			$return['status'] = "ERROR";
 			return $return;
 		}
 
-		$url = "https://www.amazon.com" . $match[1];
+		$url = "https://www.amazon.co.jp" . $match[1];
 
 		$html = $this->connect($url,$ref,null,array("no_header"=>true));
 		$data = $this->get_field($html,"signIn");
 
+		$data["create"] = 0;
 		$data["email"] = $m;
 		$data["password"] = $p;
 
-		$html = $this->connect("https://www.amazon.com/ap/signin",$url,$data,array("header_only" => true));
+		$html = $this->connect("https://www.amazon.co.jp/ap/signin",$url,$data,array("header_only" => true));
 
-		if (strpos($html, 'Location: https://www.amazon.com?ie=UTF8&ref_=nav_ya_signin&') === false) {
+		if (strpos($html, 'Location: https://www.amazon.co.jp?ie=UTF8&ref_=nav_ya_signin&') === false) {
 			$return['status'] = "DIE";
 			return $return;
 		}
 
 		if ($check_point) {
-			$html = $this->connect("https://www.amazon.com/gp/css/gc/balance?ie=UTF8&ref_=ya_view_gc");
-			if (preg_match('/<span>\$([0-9\.\,]+)<\/span/',$html,$match)) {
+			$html = $this->connect("https://www.amazon.co.jp/gp/css/gc/balance?ie=UTF8&ref_=ya_view_gc","https://www.amazon.co.jp/");
+			$html = mb_convert_encoding($html, "UTF-8", "Shift_JIS");
+			if (preg_match('/<span>￥ ([0-9]+)<\/span/',$html,$match)) {
 				$return['point'] = str_replace(",","",$match[1]);
 			} else {
-				$return['point'] = -1;
+				$return['point'] = $html;
 			}
 		}
 
